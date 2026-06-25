77ccfbd58e6596013d3410b30248071578cd9455
jms0326 <pokmeby@gmail.com>
2026-06-25 14:37:40 +0900

    style: ConsoleNewsPublisher 출력 구분선 추가 및 공백 제거(trim) 적용

--- src/oop/search/infrastructure/NaverNewsProvider.java
+++ src/oop/search/infrastructure/NaverNewsProvider.java
@@ -4,6 +4,12 @@ import oop.search.application.NewsProvider;
 import oop.search.domain.NewsCategory;
 import oop.search.domain.NewsResult;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NaverNewsProvider extends AbstractHttpScraper {
@@ -30,12 +36,66 @@ public class NaverNewsProvider extends AbstractHttpScraper {
 
     @Override
     public List<NewsResult> fetchNews(String searchQuery, int limit) {
-        return List.of();
+        String url = endpoint + "?query="
+                + URLEncoder.encode(searchQuery, StandardCharsets.UTF_8)
+                + "&display=" + limit
+                + "&sort=" + category.getQueryValue()
+                + "&start=1";
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret)
+                .build();
+
+        List<NewsResult> results = new ArrayList<>();
+        try {
+            HttpResponse<String> response = httpClient.send(
+                    request,
+                    HttpResponse.BodyHandlers.ofString()
+            );
+            String body = response.body();
+//            System.out.println("body = " + body);
+
+            // items
+            String items = body.split("items")[1]; // 0 <-> 1
+            // <- items ->
+//            System.out.println("items = " + items);
+            String[] itemArr = items.split("},");
+            for (String item : itemArr) {
+//                System.out.println("item = " + item);
+//                String title = item
+//                        .split("\"title\":\"")[1] // 0 <-> 1 -> ["title":"]
+//                        .split("\",")[0]; // ",
+                String title = cutText(item, "\"title\":\"", "\",\n");
+                String link = cutText(item, "\"link\":\"", "\",\n");
+                String description = cutText(item, "\"description\":\"", "\",\n");
+                // pubDate는 문자열 ""가 추가적으로 들어갈 염려가 없기 때문에 바로 "로 구분
+                String pubDate = cutText(item, "\"description\":\"", "\"");
+                NewsResult result = new NewsResult(title, description, link, pubDate);
+                results.add(result);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        return List.of();
+        return results;
+    }
+
+    public String cutText(String original, String prefix, String suffix) {
+        return original
+                .split(prefix)[1]
+                .split(suffix)[0];
     }
 
     public static void main(String[] args) {
         NewsProvider provider = new NaverNewsProvider();
-        List<NewsResult> results = provider.fetchNews("창억떡", 10);
-        System.out.println("results = " + results);
+        List<NewsResult> results = provider.fetchNews("프리티걸", 10);
+//        System.out.println("results = " + results);
+        for (NewsResult newsItem : results) {
+            System.out.println("newsItem = " + newsItem);
+        }
     }
 }
